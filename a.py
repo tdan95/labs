519c5fd1c2b00c09b599fbbe8a74541f2f610718
tdan95 <da_tuyakbay@kbtu.kz>
2025-01-25 12:08:22 +0500

    a.py

---
+++ a.py
@@ -0,0 +1,67 @@
+
+print("Hello, world")
+
+import sys
+print(sys.version)
+
+if 5 > 2:
+    print("Five is greater than two!")  
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+
+
+x = "awesome"
+
+def myfunc():
+x = "fantastic"
+
+myfunc()
+print("Python is " + x)
+
+
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+
+b = "Hello, World!"
+print(b[-5:-2])
+
+
+
+a = " Hello, World! "
+print(a.strip().lower())
+
+
+
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
+
+
+
+txt = "The best things in life are free!"
+print("expensive" not in txt)
+
+
